19ebcc76462b046bd65759cab009a7c0dc1405a2
Maxine De Villiers <147997765+TheMax23@users.noreply.github.com>
2025-03-20 16:00:02 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="content">
+        <p>4 days ago</p>
+        <h2>Post One</h2>
+        <p className="description">
+          Lorem ipsum dolor sit amet consectetur, Ducimus, repudiandae temporibus omnis
+          illum maxime quod deserunt eligendi dolor.
+        </p>
+      </div>
+      <div className="footer">
+        <div className="stat"><span>4m</span> READ</div>
+        <div className="stat"><span>5123</span> VIEWS</div>
+        <div className="stat"><span>32</span> COMMENTS</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
